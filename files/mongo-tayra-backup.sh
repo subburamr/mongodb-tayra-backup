b8b4ac12c4e7170ad9937f4d00103841889dd240
Subburam Rajaram <srajaram@Subburams-MacBook-Pro.local>
2016-02-18 14:39:03 +0100

    updated the script with error_exit and clean up functions

--- files/mongo-tayra-backup.sh
+++ files/mongo-tayra-backup.sh
@@ -73,7 +73,7 @@ done
 OLD_BACKUP_PROCS=$(ps aux | grep '/data/backup/mongodb'|grep -v grep|awk '{print $2}')
 if [ -n "$OLD_BACKUP_PROCS" ]; then
 	echo "Terminating old backup processes"
-	kill -15 $OLD_BACKUP_PROCS						
+	kill -15 $OLD_BACKUP_PROCS || error_exit "The previous backup processes could not be killed! Aborting"						
 fi
 
 # Do we need username/password to access database
@@ -86,12 +86,11 @@ fi
 # Archive backup and clean up tasks
 archive_prev_backup() {
 	echo "Archiving previous backup"
-	tar -P --numeric-owner --preserve-permissions -czf /data/backup/mongodb/archive/archive_$CURRENT_DATE.tar.gz /data/backup/mongodb/full /data/backup/mongodb/incremental
+	tar -P --numeric-owner --preserve-permissions -czf /data/backup/mongodb/archive/archive_$CURRENT_DATE.tar.gz /data/backup/mongodb/full /data/backup/mongodb/incremental || error_exit "Unable to archive previous backup! Aborting"
 	rm -f /data/backup/mongodb/archive/archive_latest.tar.gz
 	rm -rf /data/backup/mongodb/full/* /data/backup/mongodb/incremental/*
 	ln -s /data/backup/mongodb/archive/archive_$CURRENT_DATE.tar.gz /data/backup/mongodb/archive/archive_latest.tar.gz
 }
-
 
 # Backup Function
 runbackup() {
@@ -106,18 +105,18 @@ runbackup() {
 		echo "Executing a FULL backup"
 		LATEST_DB_TIMESTAMP=`mongo local $AUTHOPT --eval 'db.oplog.rs.find({}, {ts:1}).sort({$natural:-1}).limit(1).forEach(printjson)'|tail -1| awk -F'[(,]' '{print $2}'`
 		echo $LATEST_DB_TIMESTAMP
-		EXITSTATUS1=`echo $?`
+		#EXITSTATUS1=`echo $?`
 
 		# Write LATEST_TIMESTAMP to timestamp.out file under /opt/tayra
-		echo -n "{ \"ts\" : { \"\$ts\" : $LATEST_DB_TIMESTAMP , \"\$inc\" : 1} }" | tee /opt/tayra/timestamp.out 1> /dev/null
+		echo -n "{ \"ts\" : { \"\$ts\" : $LATEST_DB_TIMESTAMP , \"\$inc\" : 1} }" | tee /opt/tayra/timestamp.out 1> /dev/null || error_exit "Unable to update timestamp.out file! Aborting"
 
 		# Trigger mongodump to write full backup to /data/backup/mongodb/full
-		mongodump $AUTHOPT -o $FULLBACKUP_DIR
-		EXITSTATUS2=`echo $?`
+		mongodump $AUTHOPT -o $FULLBACKUP_DIR || error_exit "Unable to take backup using mongodump"
+		#EXITSTATUS2=`echo $?`
 	fi
 
 	# Tayra backup run under $TAYRA_DIR as working directory so that the timestamp.out is picked up by the processes
-	cd $TAYRA_DIR 
+	cd $TAYRA_DIR || error_exit "Cannot change directory! Aborting"
 	if [ "$SECURE" = true ] ; then
 		/opt/tayra/backup_expect.sh $BACKUP_USERNAME $BACKUP_PASSWORD $CURRENT_DATE 1> /dev/null &
 	else
@@ -125,6 +124,27 @@ runbackup() {
 	fi 
 }
 
+# Exit on Error
+error_exit() {
+	# Display error message and exit
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	clean_up 1
+}
+
+# Clean up function 
+clean_up() {
+	# Perform program exit housekeeping
+	# Optionally accepts an exit status
+	# Unset variables initialized
+	echo "Unsetting variables"
+	for VARIABLES in FULLBACKUP_FREQUENCY FULL_BACKUP FULLBACKUP_DIR LATEST_ARCHIVE TAYRA_DIR CURRENT_DATE LATEST_ARCHIVE_DATE DATE_DIFFERENCE OLD_BACKUP_PROCS LATEST_DB_TIMESTAMP IS_MASTER
+	do
+		unset $VARIABLES
+	done
+	exit $1
+}
+
+trap clean_up SIGHUP SIGINT SIGTERM
 
 if [ -f "$LATEST_ARCHIVE" ]; then
 	LATEST_ARCHIVE_DATE=`ls -ld --time-style="+%F" $LATEST_ARCHIVE|awk '{print $6}'`
@@ -144,16 +164,10 @@ IS_MASTER=`mongo --quiet --eval "d=db.isMaster(); print( d['ismaster'] );"` #
 if [ "$IS_MASTER" = true ] ; then
 	# Try stepping down
 	##  	mongo --quiet --eval "rs.stepDown();"
-	echo "Could not proceed with the backup as I am the Primary node"
+	echo "Could not proceed with the backup as I am the Primary node" && error_exit "Unable to take backup using mongodump. Aborting"
 else 
 	# I am a secondary node, safe to proceed with the backup 
 	runbackup
 fi
 
-
-# Unset variables initialized
-echo "Unsetting variables"
-for VARIABLES in FULLBACKUP_FREQUENCY FULL_BACKUP FULLBACKUP_DIR LATEST_ARCHIVE TAYRA_DIR CURRENT_DATE LATEST_ARCHIVE_DATE DATE_DIFFERENCE OLD_BACKUP_PROCS LATEST_DB_TIMESTAMP IS_MASTER
-do
-	unset $VARIABLES
-done
+clean_up
